1b7085bad0c4b2bb1a0b817b3523829f1f9f6c9d
Alan1321 <alansubedi123@gmail.com>
2022-10-07 14:50:53 -0500

    It Works

--- src/components/DisplayColor.js
+++ src/components/DisplayColor.js
@@ -1,6 +1,14 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { getRandomHex, shuffledArray } from "../utils/HelperMethod";
 import styled from "styled-components";
+
+const OuterContainer = styled.div`
+display:flex;
+flex-direction:column;
+justify-content:center;
+text-align:center;
+align-items:center;
+`
 
 const Container = styled.div`
 width:100%;
@@ -11,7 +19,6 @@ justify-content:center;
 text-align:center;
 align-items:center;
 `
-
 const Color = styled.div`
 height:100px;
 width:100px;
@@ -21,39 +28,47 @@ const Buttons = styled.div`
 margin-top:1rem;
 `
 
+let hex; 
+let randomNum;
+let toPrint;
 
 const DisplayColor = () =>{
 
-    let hex = [getRandomHex(), getRandomHex(), getRandomHex()]
-    let randomNum = shuffledArray();
+    if(typeof hex === 'undefined'){   
+        console.log('hello world', hex)     
+        hex = [getRandomHex(), getRandomHex(), getRandomHex()]
+        randomNum = shuffledArray();
+    }
+
     const [color, setColor] = useState(hex[Math.floor(Math.random()*3)]);
-    
+    const [print, setPrint] = useState(null)
+
     const clickHandler = (e) =>{
         if(color === e.target.value){
             console.log('Correct!!')
-            hex = [[getRandomHex(), getRandomHex(), getRandomHex()]]
-            //console.log(hex)
-
-            setColor(hex[Math.floor(Math.random()*3)])
             randomNum = shuffledArray();
+            hex = [getRandomHex(), getRandomHex(), getRandomHex()]
+            const randomColor = hex[Math.floor(Math.random()*3)]
+            setColor(randomColor)
+            setPrint('Correct!!')
         }else{
-            //console.log('Incorrect :(')
+            console.log("Incorrect")
+            setPrint('Incorrect :(')
         }
-        console.log(color)
-        console.log(e.target.value)
     }
 
-
     return(
-        <Container>
-            <Color color={color}/>
-            {console.log(color)}
-            <Buttons>
-                <button onClick={clickHandler} value={hex[randomNum[0]]}>{hex[randomNum[0]]}</button>
-                <button onClick={clickHandler} value={hex[randomNum[1]]}>{hex[randomNum[1]]}</button>
-                <button onClick={clickHandler} value={hex[randomNum[2]]}>{hex[randomNum[2]]}</button>
-            </Buttons>
-        </Container>
+        <OuterContainer>
+            <Container>
+                <Color color={color}/>
+                <Buttons>
+                    <button onClick={clickHandler} value={hex[randomNum[0]]}>{hex[randomNum[0]]}</button>
+                    <button onClick={clickHandler} value={hex[randomNum[1]]}>{hex[randomNum[1]]}</button>
+                    <button onClick={clickHandler} value={hex[randomNum[2]]}>{hex[randomNum[2]]}</button>
+                </Buttons>
+                {print !== null && <div>${print}</div>}
+            </Container>
+        </OuterContainer>
     )
 }
 
