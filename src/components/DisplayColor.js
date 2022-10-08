b2ea2c0f2a7a15846eedab98ce93a65d5edc46ac
Alan1321 <alansubedi123@gmail.com>
2022-10-08 01:31:30 -0500

    Added title, score, better overall styling --final commit

--- src/components/DisplayColor.js
+++ src/components/DisplayColor.js
@@ -8,6 +8,8 @@ flex-direction:column;
 justify-content:center;
 text-align:center;
 align-items:center;
+height:100vh;
+width:100vw;
 `
 
 const Container = styled.div`
@@ -18,15 +20,33 @@ flex-direction:column;
 justify-content:center;
 text-align:center;
 align-items:center;
+margin-top:-50px;
 `
+const Title = styled.div`
+margin-top:30px;
+font-weight:bold;
+font-size:30px;
+`
+
 const Color = styled.div`
 height:100px;
 width:100px;
 background-color:${(props)=>props.color};
 `
+const Score = styled.div`
+margin-bottom:2rem;
+font-weight:bold;
+font-size:30px;
+`
+const Button = styled.button`
+margin-right:10px;
+`
+
 const Buttons = styled.div`
 margin-top:1rem;
+width:100%;
 `
+
 
 let hex; 
 let randomNum;
@@ -41,7 +61,8 @@ const DisplayColor = () =>{
     }
 
     const [color, setColor] = useState(hex[Math.floor(Math.random()*3)]);
-    const [print, setPrint] = useState(null)
+    const [print, setPrint] = useState(null);
+    const [score, setScore] = useState(0);
 
     const clickHandler = (e) =>{
         if(color === e.target.value){
@@ -51,6 +72,7 @@ const DisplayColor = () =>{
             const randomColor = hex[Math.floor(Math.random()*3)]
             setColor(randomColor)
             setPrint('Correct!!')
+            setScore(score + 1)
         }else{
             console.log("Incorrect")
             setPrint('Incorrect :(')
@@ -59,14 +81,16 @@ const DisplayColor = () =>{
 
     return(
         <OuterContainer>
+            <Title>Guess the Color</Title>
             <Container>
+                <Score>Score: {score}</Score>
                 <Color color={color}/>
                 <Buttons>
-                    <button onClick={clickHandler} value={hex[randomNum[0]]}>{hex[randomNum[0]]}</button>
-                    <button onClick={clickHandler} value={hex[randomNum[1]]}>{hex[randomNum[1]]}</button>
-                    <button onClick={clickHandler} value={hex[randomNum[2]]}>{hex[randomNum[2]]}</button>
+                    <Button onClick={clickHandler} value={hex[randomNum[0]]}>{hex[randomNum[0]]}</Button>
+                    <Button onClick={clickHandler} value={hex[randomNum[1]]}>{hex[randomNum[1]]}</Button>
+                    <Button onClick={clickHandler} value={hex[randomNum[2]]}>{hex[randomNum[2]]}</Button>
                 </Buttons>
-                {print !== null && <div>${print}</div>}
+                {print !== null && <div>{print}</div>}
             </Container>
         </OuterContainer>
     )
